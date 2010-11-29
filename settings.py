6fa5f2487a7ca676d78f19fa82d28e2f01f5057e
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2010-11-29 12:10:29 +0000

    Ignore compiled Python files, and add Geodjango

--- settings.py
+++ settings.py
@@ -89,9 +89,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.gis',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'manager',
+    'logger',
 )
