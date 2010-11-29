2c7a14f5438cc8afcc5de9583441685e0d9d390f
Tim Fernando <tim.fernando@oucs.ox.ac.uk>
2010-11-29 15:13:10 +0000

    Adding admin UI to all models

--- logger/admin.py
+++ logger/admin.py
@@ -5,5 +5,5 @@ from django.db.models import get_app, get_models
 Iterates through models in an app and adds them to Admin UI
 """
 
-for model in get_models(get_app("manager")):
+for model in get_models(get_app("logger")):
 	admin.site.register(model)
