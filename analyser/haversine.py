0fd49596ff55255b23059a8d6a1214679de482ab
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2010-11-30 16:40:57 +0000

    speed up analysis

---
+++ analyser/haversine.py
@@ -0,0 +1,22 @@
+import math
+
+
+def haversine(origin, destination, R=6371):
+    """
+    Returns the distance between two points using the haversine formula
+    
+    http://www.platoscave.net/blog/2009/oct/5/calculate-distance-latitude-longitude-python/
+    """
+    
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
--- analyser/management/commands/analyse_clusters.py
+++ analyser/management/commands/analyse_clusters.py
@@ -5,12 +5,13 @@ from django.contrib.gis.measure import Distance
 from geopy import geocoders
 
 from analyser.models import Cluster
+from analyser.haversine import haversine
 from logger.models import Location
 from manager.models import Device
 
 from datetime import datetime, timedelta
 
-THRESHOLD_DISTANCE = 30 # The radius points are allowed to be from each other to
+THRESHOLD_DISTANCE = 0.1 # The radius points are allowed to be from each other to
                         # be considered as part of a cluster
 THRESHOLD_TIME = 5 # How many minutes in the same place for this to be a cluster
 THRESHOLD_ACCURACY = 6 # Only consider GPS points below this level of accuracy
@@ -33,22 +34,23 @@ class Command(BaseCommand):
                 # Start a new cluster to be considered if the current one is empty
                 if len(current_cluster_locations) == 0:
                     current_cluster_locations.add(location)
+                
                 else:
+                    
                     # Check whether this point could be considered to be a member of this cluster
                     current_cluster = MultiPoint([l.location for l in current_cluster_locations])
-                    
-                    # Get all objects within THRESHOLD_DISTANCE metres of here
-                    # Is this location included in it?
-                    print location.sent_date_time
-                    if location in Location.objects.filter(location__distance_lt=(current_cluster.centroid, Distance(m=THRESHOLD_DISTANCE))):
+                    distance = haversine(current_cluster.centroid, location.location)
+                    if distance < THRESHOLD_DISTANCE:
                         # Good, our cluster just got bigger
                         current_cluster_locations.add(location)
-                        last_chance = False
+                        last_chance = True
                     else:
                         # If this is the first outlier, then just move on to the next one
                         if not last_chance:
-                            last_chance = True
-                        else:
+                            last_chance = False
+                        # If we've no more chances, and we haven't had a point in
+                        # this cluster for THRESHOLD_TIME minutes, then give it up
+                        elif max([l.sent_date_time for l in current_cluster_locations]) + timedelta(minutes=THRESHOLD_TIME) < location.sent_date_time:
                             # We've missed our chances, that's the end of this cluster
                             # Figure out if we've stayed in this spot long enough
                             start = min([l.sent_date_time for l in current_cluster_locations])
@@ -66,8 +68,9 @@ class Command(BaseCommand):
                             
                             # Clear and go on to our next cluster
                             current_cluster_locations = set((location,))
+                            print location.sent_date_time
                             print "New cluster!"
-                            last_chance = False
+                            last_chance = True
                     
                 location.save()
             
