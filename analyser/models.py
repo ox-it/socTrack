fd9257e8455bd9c8fa23cea4dd57e52d23062bd7
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2010-12-03 13:41:24 +0000

    Add location analysis reports

--- analyser/models.py
+++ analyser/models.py
@@ -17,8 +17,12 @@ class Cluster(models.Model):
         end = datetime.combine(deployment.survey_end, time(23, 59, 59))
         clusters = []
         for cluster in Cluster.objects.filter(device=deployment.device):
-            youngest = min([l.sent_date_time for l in cluster.locations.all()])
-            eldest = max([l.sent_date_time for l in cluster.locations.all()])
-            if (youngest > start and eldest < end) or (youngest < start and eldest > start) or (youngest < end and eldest > end):
+            if (cluster.youngest() > start and cluster.eldest() < end) or (cluster.youngest() < start and cluster.eldest() > start) or (cluster.youngest() < end and cluster.eldest() > end):
                 clusters.append(cluster)
         return clusters
+    
+    def youngest(self):
+        return min([l.sent_date_time for l in self.locations.all()])
+    
+    def eldest(self):
+        return max([l.sent_date_time for l in self.locations.all()])
