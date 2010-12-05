ca1a9b6af240b7542b44468b5ca0355adf48578b
Tim Fernando <info@timfernando.com>
2010-12-05 17:22:08 +0000

    Moar corrections

--- analyser/management/commands/analyse_clusters.py
+++ analyser/management/commands/analyse_clusters.py
@@ -60,33 +60,7 @@ class Command(BaseCommand):
                 location.end_date_time = location.sent_date_time
                
             # Keep iterating through gradually decreasing number of points until there has been no change since the last iteration  
-            points_merged = True 
-            iterations = 0
-            while points_merged == True:
-                points_merged = False
-                iterations += 1
-                j = 0
-                while j < len(locations) -1:
-                    # Calculate time delta between the two points
-                    dTime = locations[j+1].sent_date_time - locations[j].end_date_time    
-                    if dTime < timedelta(minutes=THRESHOLD_TIME):
-                        # Find distance betweeen the two points                    
-                        distance = haversine(locations[j].location, locations[j+1].location)
-                        if distance < THRESHOLD_DISTANCE:
-                            # Find average of the two points
-                            locations[j].location = MultiPoint([locations[j].location, locations[j+1].location]).centroid
-                            # Save average point and set the point end_time to the send time of the second point
-                            locations[j].altitude = (locations[j].altitude + locations[j+1].altitude) / 2
-                            locations[j].end_date_time = locations[j+1].end_date_time
-                            locations[j].speed = (locations[j].speed + locations[j+1].speed) /2
-                            # Add all the collected points to the new cluster
-                            for point in locations[j+1].points:
-                                locations[j].points.append(point)
-                            # Remove the second location as it has been merged with the first    
-                            del locations[j+1]
-                            points_merged = True
-                    j += 1
-                    
+            locations, iterations = merge_points(locations)
             locations = [location for location in locations if (location.end_date_time - location.sent_date_time) > timedelta(seconds=THRESHOLD_MIN_TIME)]
         
             locations, iterations = merge_points(locations) 
@@ -101,10 +75,10 @@ class Command(BaseCommand):
                         place = "Unable to geocode"
                 """
             
-            c = Cluster(geocoded=place, device=device, location=location.location, speed=location.speed, altitude=location.altitude)
-            c.save()
-            c.locations = location.points
-            c.save()
+                c = Cluster(geocoded=place, device=device, location=location.location, speed=location.speed, altitude=location.altitude)
+                c.save()
+                c.locations = location.points
+                c.save()
         
             
             print device.local_id + " points created: " + str(len(locations)) + " iterations: " + str(iterations)
