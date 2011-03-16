d70e6359d5f4d0fcea2cd47558d240ed0a55ef0c
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-03-16 13:12:03 +0000

    Don't include raw data in report dump

--- analyser/management/commands/dump_reports.py
+++ analyser/management/commands/dump_reports.py
@@ -22,7 +22,9 @@ class Command(BaseCommand):
             ddate = deployment.survey_start
             while ddate <= deployment.survey_end and ddate < date.today():
                 with open(os.path.join(ddir, ddate.isoformat() + '.kml'), 'w') as fd:
+                    context = context_for_kml(deployment, ddate)
+                    context['locations'] = []
                     kml = render_to_string('analyser/clusters.kml',
-                                           context_for_kml(deployment, ddate))
+                                           context)
                     fd.write(kml)
                 ddate += timedelta(days=1)
