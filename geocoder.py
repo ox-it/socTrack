fa35cec0e7623eeac476c3123b621b826e06ede0
Chris Northwood <christopher.northwood@oucs.ox.ac.uk>
2011-06-27 15:04:56 +0100

    Fix geocoder

--- geocoder.py
+++ geocoder.py
@@ -4,8 +4,12 @@ import simplejson
 
 from secrets import secrets
 
-YAHOO_PLACEFINDER_URL = 'http://where.yahooapis.com/geocode?appid=%s&flags=GJTX&gflags=AR&location=%%%f%%%%20%%%f' % secrets['YAHOO']
+YAHOO_PLACEFINDER_URL = 'http://where.yahooapis.com/geocode?appid=%s&flags=GJTX&gflags=AR&location=%%f%%%%20%%f' % secrets['YAHOO']
 
 def reverse_geocode(lat, lon):
     geocode = simplejson.load(urlopen(YAHOO_PLACEFINDER_URL % (lat, lon)))
-    return geocode['ResultSet']['Results'][0]['name']
+    return '%s, %s%s, %s' % (
+        geocode['ResultSet']['Results'][0]['line1'],
+        (geocode['ResultSet']['Results'][0]['level4'] + ', ') if geocode['ResultSet']['Results'][0]['level4'] else '',
+        geocode['ResultSet']['Results'][0]['level3'],
+        geocode['ResultSet']['Results'][0]['level2'])
